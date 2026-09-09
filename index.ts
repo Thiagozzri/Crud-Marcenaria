0691593b85c406f66073c27c045be4fefcb7da7f
Thiago <thiagoppelizzari@gmail.com>
2026-09-09 08:48:20 -0300

    index

--- index.ts
+++ index.ts
@@ -4,3 +4,6 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 
 registerRootComponent(App);
+
+// O Expo local usa registerRootComponent; o Expo Snack procura um export padrão.
+export default App;
